cf1d246818e3c2864b46c2ddcf6d330fd0391dcf
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2025-10-27 16:36:26 +0300

    feature: Feature_importance do modelo'

--- src/train.py
+++ src/train.py
@@ -211,3 +211,14 @@ with mlflow.start_run():
         ])
 
     plt.show()
+
+
+# Feature_importance do modelo
+features_names = (model[0][0].transform(X_train[features]).columns.tolist())
+
+feature_importance = pd.Series(model[0][-1].feature_importances_,
+                               index=features_names)
+
+feature_importance.sort_values(ascending=False)
+
+print(feature_importance.sort_values(ascending=False))
