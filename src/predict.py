4d88e555765b6efe5ef2c34d379d0c5d4fcc8344
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2025-10-27 06:11:08 +0300

    feature: Import das features do mlflow

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import mlflow
-import sklearn.mlflow
+import mlflow.sklearn
 
 
 
 
+
+
+# Import do modelo do mlflow
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+model = mlflow.sklearn.log_model("models:/model_student_depression/1")
+print(model)
 
-# Import do modelo
-
+# Import das features do mlflow
+features = model.feature_names_in_
