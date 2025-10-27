85cef307794b85fde3ab0cea943ee2fa3a427e0e
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2025-10-27 17:03:47 +0300

    feature: Capturar sempre a versão mais recente do  modelo no mlfow

--- src/predict.py
+++ src/predict.py
@@ -9,7 +9,6 @@ df = pd.read_csv("../data/student_depression_dataset.csv")
 
 # Criar função para extrair horas da variável 'Sleep Duration'
 def process_sleep_duration(series):
-
     def extract_hours(s):
         match = re.search(r"(\d+(\.\d+)?)", str(s))
         return float(match.group(1)) if match else np.nan
@@ -21,9 +20,13 @@ def process_sleep_duration(series):
         print("Valores numéricos extraídos, preenchidos com média.")
     return series.values.reshape(-1, 1)
 
+# Capturar sempre a versão mais recente do  modelo no mlfow
+models = mlflow.search_registered_models(filter_string="name = 'model_student_depression' ")
+latest_versions = max([i.version for in models[0].latest_versions[]])
+
 # Import do modelo do mlflow
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
-model = mlflow.sklearn.load_model("models:/model_student_depression/2")
+model = mlflow.sklearn.load_model(f"models:/model_student_depression/{latest_versions}")
 print(model)
 
 # Import das features do mlflow
